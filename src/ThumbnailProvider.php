8817b5cb04ce88b56f287ad562fe849ceea267d3
Ross Riley <riley.ross@gmail.com>
2014-06-06 17:25:20 +0100

    tweak to route matching

--- src/ThumbnailProvider.php
+++ src/ThumbnailProvider.php
@@ -13,7 +13,7 @@ class ThumbnailProvider implements ControllerProviderInterface
         // creates a new controller based on the default route
         $controllers = $app['controllers_factory'];
 
-        $controllers->get('{thumb}', function (Application $app) {
+        $controllers->get('/thumbs/{thumb}', function (Application $app) {
             $request = $app['request'];
             $action = new ThumbnailResponder($app, $request);
             if($response = $action->respond()) {
