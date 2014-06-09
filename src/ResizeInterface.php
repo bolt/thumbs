2604744bc26a804309d962f9132ef85c1136985f
Ross Riley <riley.ross@gmail.com>
2014-06-09 11:20:20 +0100

    better documentation for interface

--- src/ResizeInterface.php
+++ src/ResizeInterface.php
@@ -2,10 +2,21 @@
 namespace Bolt\Thumbs;
 use Symfony\Component\HttpFoundation\File\File;
 
-
+/**
+ * Minimum functionality required to support Bolt thumbnail functionality.
+ *
+ **/
+ 
 interface ResizeInterface
 {
     
+    /**
+     * Specifies an array of command -> method mappings for example the defaults are:
+     *     ['c'=>'crop','r'=>'resize','b'=>'border','f'=>'fit']
+     * Any others can be specified and if the method is callable they will work.
+     * 
+     * @return array
+     **/
     public function provides();
 
     public function setSource(File $source);
