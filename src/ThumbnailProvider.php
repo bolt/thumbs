e0abf8c9df7b039eb1a2fc9b342ce2da5aba7751
Ross Riley <riley.ross@gmail.com>
2014-06-17 16:07:29 +0100

    revert,

--- src/ThumbnailProvider.php
+++ src/ThumbnailProvider.php
@@ -23,7 +23,14 @@ class ThumbnailProvider implements ServiceProviderInterface, ControllerProviderI
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/{thumb}', function (Application $app) {
-            if($response = $app['thumbnails']->respond()) {
+
+            if(isset($app['thumbnails'])) {
+                $action = $app['thumbnails'];
+            } else {
+                $action = new ThumbnailResponder($app, $app['request']);
+            }
+
+            if($response = $action->respond()) {
                 return $response;
             } else {
                 $app->pass();
